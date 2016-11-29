18db5f8021492a2615dbbe8bf57cbbb28e1f19f9
Jïn Muhjo <fayte@ayphix.com>
2016-11-29 12:11:33 -0500

    Added change log and modified Main.php to show a small showcase for the skin system

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,16 +2,13 @@
 
 class Main extends CI_Controller {
     
-    public $social;
-    
     public function __construct() {
         parent::__construct();
-        $this->globals->set("social", $this->db->select("*")->from("social")->get()->result());
-        $this->breadcrumb->reset("Home", "<span class='fa fa-angle-right'></span>");
     }
 
     public function index() {
-    	$this->breadcrumb->add("News");
-        $this->pages->call($this);
+        $this->skin->load_skin("default");
+        $this->skin->load_page("standard");
+        $this->skin->show($this);
     }
 }
